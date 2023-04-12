779b0e2b14667d85b1c9a3ca0c2c1fb2055fb309
Bryce Watson <brycewatson315@gmail.com>
2023-04-12 15:50:58 -0400

    icon hover states

--- src/Figma.js
+++ src/Figma.js
@@ -9,26 +9,37 @@ export default function Figma(props) {
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   useFrame((state, delta) => (
-    group.current.rotation.z += (delta/6),
+    group.current.rotation.z += (delta / 6),
     group.current.rotation.x = 1.8
-    
-    ))
+
+  ))
 
   return (
-    <group 
-    ref={group} 
-    {...props} 
-    dispose={null} 
-    scale={clicked ? 60 : 50}
-    onClick={(event) => click(!clicked)}
-    onPointerOver={(event) => hover(true)}
-    onPointerOut={(event) => hover(false)}
+    <group
+      ref={group}
+      {...props}
+      dispose={null}
+      scale={clicked ? 60 : 50}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+
     >
-      <mesh castShadow receiveShadow geometry={nodes.Blue.geometry} material={materials['SVGBlue']} />
-      <mesh castShadow receiveShadow geometry={nodes.Green.geometry} material={materials['SVGGreen']} />
-      <mesh castShadow receiveShadow geometry={nodes.Orange.geometry} material={materials['SVGOrange']} />
-      <mesh castShadow receiveShadow geometry={nodes.Red.geometry} material={materials['SVGRed']} />
-      <mesh castShadow receiveShadow geometry={nodes.Purple.geometry} material={materials['SVGPurple']} />
+      <mesh castShadow receiveShadow geometry={nodes.Blue.geometry} material={materials['SVGBlue']}>
+        <meshStandardMaterial color={hovered ? "#107198" : '#1ABCFE'} />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={nodes.Green.geometry} material={materials['SVGGreen']} >
+        <meshStandardMaterial color={hovered ? "#067C4F" : '#0ACF83'} />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={nodes.Orange.geometry} material={materials['SVGOrange']} >
+        <meshStandardMaterial color={hovered ? "#99443B" : '#FF7262'} />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={nodes.Red.geometry} material={materials['SVGRed']} >
+        <meshStandardMaterial color={hovered ? "#912F12" : '#F24E1E'} />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={nodes.Purple.geometry} material={materials['SVGPurple']} >
+        <meshStandardMaterial color={hovered ? "#613599" : '#A259FF'} />
+      </mesh>
     </group>
   )
 }
--- src/VSCode.js
+++ src/VSCode.js
@@ -9,24 +9,32 @@ export default function VSCode(props) {
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
   useFrame((state, delta) => (
-    group.current.rotation.z += (delta/6),
+    group.current.rotation.z += (delta / 6),
     group.current.rotation.x = 1.8
-    ))
+  ))
 
   return (
-    <group 
-    ref={group} 
-    {...props} 
-    dispose={null} 
-    scale={clicked ? 60 : 50}
-    onClick={(event) => click(!clicked)}
-    onPointerOver={(event) => hover(true)}
-    onPointerOut={(event) => hover(false)}
+    <group
+      ref={group}
+      {...props}
+      dispose={null}
+      scale={clicked ? 60 : 50}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
     >
-      <mesh castShadow receiveShadow geometry={nodes.Top.geometry} material={materials['Blue']} />
-      <mesh castShadow receiveShadow geometry={nodes.Bottom.geometry} material={materials['Blue']} />
-      <mesh castShadow receiveShadow geometry={nodes.Arc.geometry} material={materials['LighterBlue']} />
-      <mesh castShadow receiveShadow geometry={nodes.Anchor.geometry} material={materials['LightestBlue']} />
+      <mesh castShadow receiveShadow geometry={nodes.Top.geometry} material={materials['Blue']} >
+        <meshStandardMaterial color={hovered ? "#003D65" : '#0065A9'} />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={nodes.Bottom.geometry} material={materials['Blue']} >
+        <meshStandardMaterial color={hovered ? "#003D65" : '#0065A9'} />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={nodes.Arc.geometry} material={materials['LighterBlue']} >
+        <meshStandardMaterial color={hovered ? "#00497A" : '#007ACC'} />
+      </mesh>
+      <mesh castShadow receiveShadow geometry={nodes.Anchor.geometry} material={materials['LightestBlue']} >
+        <meshStandardMaterial color={hovered ? "#135E90" : '#1F9CF0'} />
+      </mesh>
     </group>
   )
 }
