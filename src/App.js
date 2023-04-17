31f1d54fd3e8696da388dfbd95d08d19079f3829
Bryce Watson <brycewatson315@gmail.com>
2023-04-17 16:16:23 -0400

    icons are now swappable

--- src/App.js
+++ src/App.js
@@ -3,10 +3,44 @@ import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Stars } from '@react-three/drei'
 import Figma from "./Figma";
 import VSCode from "./VSCode";
+import Sketch from "./Sketch";
+// import Postman from "./Postman";
 import Text from './Text';
 import Ground from './Ground';
 import Label from './Label';
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
+import { useControls } from 'leva'
+import { useGLTF } from '@react-three/drei'
+import { MathUtils } from 'three';
+
+
+
+const DESIGNMODELS = {
+  Figma: "./Figma.gltf",
+  Sketch: "./Sketch.gltf",
+}
+
+const DEVMODELS = {
+  VSCode: "./VSCode.gltf",
+  Postman: "./Postman.gltf",
+}
+
+function DesignModel({ url, ...props }) {
+  const { scene } = useGLTF(url)
+  // <primitive object={...} mounts an already existing object
+  return <primitive object={scene} {...props} scale={25} />
+}
+
+function DevModel({ url, ...props }) {
+  const { scene } = useGLTF(url)
+
+  if (url === "./Postman.gltf") {
+    return <primitive object={scene} {...props} scale={0.7}/>
+  } else {
+    return <primitive object={scene} {...props} scale={25} />
+  }
+  // <primitive object={...} mounts an already existing object
+}
 
 
 function Box(props) {
@@ -34,6 +68,9 @@ function Box(props) {
 
 export default function App() {
 
+  const { Design } = useControls({ Design: { value: 'Figma', options: Object.keys(DESIGNMODELS) } });
+  const { Development } = useControls({ Development: { value: 'Postman', options: Object.keys(DEVMODELS) } });
+
   return (
     <Canvas>
       <color attach="background" args={['black']} />
@@ -41,9 +78,11 @@ export default function App() {
       <spotLight position={[-10, 40, 30]} angle={0.08} penumbra={0.8} />
       {/* <pointLight position={[-0, -0, -10]} /> */}
       <Text />
-      <Figma />
-      <VSCode />
-      <Stars />
+      {/* <Sketch />
+      <VSCode /> */}
+      <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+      <DevModel position={[1.4, 0.25, 0]} url={DEVMODELS[Development]} />
+      <DesignModel position={[-1.4, 0.25, 0]} url={DESIGNMODELS[Design]} />
       <Label text="Design" x={-2.4} y={-1.3} />
       <Label text="Develop" x={0.5} y={-1.3} />
       <Ground mirror={1} blur={[12000, 1200]} mixBlur={12} mixStrength={1.5} rotation={[-Math.PI / 2, 0, Math.PI / 2]} position-y={-2} scale={20} />
---
+++ src/Sketch.js
@@ -0,0 +1,59 @@
+import React, { useRef, useState } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
+import Label from './Label'
+
+export default function Sketch(props) {
+    const group = useRef()
+    const { nodes, materials } = useGLTF('/Sketch.gltf')
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+
+
+    useFrame((state, delta) => (
+        // group.current.rotation.z += (delta / 6),
+        group.current.rotation.x = Math.PI / 2
+    ))
+
+    return (
+        <group
+            ref={group}
+            {...props}
+            dispose={null}
+            scale={clicked ? 60 : 40}
+            onClick={(event) => click(!clicked)}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}
+            position={[-2, -1, 0]}
+        >
+            <mesh castShadow receiveShadow geometry={nodes.CenterBottom.geometry} material={materials['Yellow']}>
+                <meshStandardMaterial color={hovered ? "#986800" : '#FDAD00'} />
+            </mesh>
+            <mesh castShadow receiveShadow geometry={nodes.LeftBottom.geometry}>
+                <meshStandardMaterial color={hovered ? "#8C4100" : '#EA6C00'} />
+            </mesh>
+            <mesh castShadow receiveShadow geometry={nodes.RightBottom.geometry}>
+                <meshStandardMaterial color={hovered ? "#8C4100" : '#EA6C00'} />
+            </mesh>
+
+            <mesh castShadow receiveShadow geometry={nodes.TopFurtherLeft.geometry}>
+                <meshStandardMaterial color={hovered ? "#986800" : '#FDAD00'} />
+            </mesh>
+            <mesh castShadow receiveShadow geometry={nodes.TopFurtherRight.geometry}>
+                <meshStandardMaterial color={hovered ? "#986800" : '#FDAD00'} />
+            </mesh>
+            <mesh castShadow receiveShadow geometry={nodes.TopLeft.geometry} material={materials['SVGBlue']}>
+                <meshStandardMaterial color={hovered ? "#987E1D" : '#FDD231'} />
+            </mesh>
+            <mesh castShadow receiveShadow geometry={nodes.TopRight.geometry} material={materials['SVGBlue']}>
+                <meshStandardMaterial color={hovered ? "#987E1D" : '#FDD231'} />
+            </mesh>
+            <mesh castShadow receiveShadow geometry={nodes.Center.geometry} material={materials['BrightYellow']}>
+                <meshStandardMaterial color={hovered ? "#988F6E" : '#FEEEB7'} />
+            </mesh>
+        </group>
+
+    )
+}
+
+useGLTF.preload('/Figma.gltf')
